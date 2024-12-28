bdedad3d3dbd78fe71de4b426cf94549bc33f2ca
Ravi Yadav <148179385+cipherravi@users.noreply.github.com>
2024-12-29 00:49:07 +0530

    fixed some performance issues

--- Projects/Foodie/src/Components/RestaurantMenuCategory.jsx
+++ Projects/Foodie/src/Components/RestaurantMenuCategory.jsx
@@ -2,44 +2,46 @@ import { useState } from "react";
 import RestaurantMenuCard from "./RestaurantMenuCard";
 
 const RestaurantMenuCategory = ({ dataForMenu }) => {
+  const [rotatedIndexes, setRotatedIndexes] = useState({});
+  const [collapsedIndexes, setCollapsedIndexes] = useState({});
+
+  const handleClick = (index) => {
+    setRotatedIndexes((prev) => ({ ...prev, [index]: !prev[index] }));
+    setCollapsedIndexes((prev) => ({ ...prev, [index]: !prev[index] }));
+  };
+
   return dataForMenu.map((item, index) => {
     const itemCards = item?.card?.card.itemCards;
-    // console.log(itemCards);
     const title = item?.card?.card?.title;
-    // console.log(title);
-    const [isRotated, setIsRotated] = useState(false);
-    const [isCollapsed, setIsCollapsed] = useState(false);
-    const handleClick = () => {
-      setIsRotated(!isRotated);
-      setIsCollapsed(!isCollapsed); // Toggle state between true/false
-    };
-    return title == undefined ? null : (
-      <>
+
+    return title == undefined || title == "Top Picks" ? null : (
+      <div key={index}>
         <div className="menu-heading">
           <span>{title} </span>
           <span
-            className="dropdown"
-            onClick={handleClick}
+            className="dropdown "
+            onClick={() => handleClick(index)}
             style={{
               display: "inline-block", // Necessary for applying transform
               transition: "transform 0.2s ease", // Smooth rotation transition
-              transform: isRotated ? "rotate(90deg)" : "rotate(-90deg)", // Rotate by 180 degrees when clicked
+              transform: rotatedIndexes[index]
+                ? "rotate(0deg)"
+                : "rotate(180deg)", // Rotate by 180 degrees when clicked
             }}
           >
-            {">"}
+            <i className="fa-solid fa-chevron-down"></i>
           </span>
         </div>
         <div
           className="menu-section"
           style={{
-            height: isCollapsed ? "0" : "", // Set height to 0 or 200px based on the state
+            height: collapsedIndexes[index] ? "0" : "", // Set height to 0 or 200px based on the state
             overflow: "hidden", // Hide content when the height is 0
             transition: "height 0.3s ease", // Smooth transition effect for height change
           }}
         >
           {itemCards != undefined
             ? itemCards.map((item, index) => {
-                console.log(item);
                 return (
                   <RestaurantMenuCard
                     key={item?.card?.info?.id || index}
@@ -49,7 +51,7 @@ const RestaurantMenuCategory = ({ dataForMenu }) => {
               })
             : null}
         </div>
-      </>
+      </div>
     );
   });
 };
